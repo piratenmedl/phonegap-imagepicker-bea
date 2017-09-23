c8a434bd9f58ba9446bd86a88f2b3678c8f1a587
piratenmedl <mail@bealisa.com>
2017-09-23 20:35:12 +0200

    test

--- src/ios/ELCImagePicker/ELCAssetTablePicker.m
+++ src/ios/ELCImagePicker/ELCAssetTablePicker.m
@@ -26,25 +26,14 @@
 
 - (id)init
 {
-    self = [super init];
+    //self = [super init];
+    self = [super initWithRootViewController:rootViewController];
     if (self) {
         //Sets a reasonable default bigger then 0 for columns
         //So that we don't have a divide by 0 scenario
         self.columns = 4;
         self.assetPadding = 2;
         //self.total = 49;
-    }
-    return self;
-}
-
-- (id)initImagePicker
-{
-    ELCAlbumPickerController *albumPicker = [[ELCAlbumPickerController alloc] initWithStyle:UITableViewStylePlain];
-
-    self = [super initWithRootViewController:albumPicker];
-    if (self) {
-        self.total = 49;
-        [albumPicker setParent:self];
     }
     return self;
 }
@@ -199,8 +188,8 @@
     //self.vorh = [[options objectForKey:@"vorh"] integerValue];
     //self.total = [[options objectForKey:@"total"] integerValue];
 	
-	//[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.totalSelectedAssets]];
-	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.total]];
+	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.totalSelectedAssets]];
+	//[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.total]];
 }
 
 - (void)selectAllAction:(id)sender
