8e5b5ca991d130e32e558267d0b981aa00ad944b
piratenmedl <mail@bealisa.com>
2017-09-23 21:20:48 +0200

    test

--- src/ios/ELCImagePicker/ELCAssetTablePicker.m
+++ src/ios/ELCImagePicker/ELCAssetTablePicker.m
@@ -185,7 +185,7 @@
     //self.total = [[options objectForKey:@"total"] integerValue];
 	
 	//[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.totalSelectedAssets]];
-	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), imagePicker.total]];
+	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), picker.total]];
 }
 
 - (void)selectAllAction:(id)sender
