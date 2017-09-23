df0334cff38ae95698d02d095f6bd81e69eb0a45
piratenmedl <mail@bealisa.com>
2017-09-23 20:26:50 +0200

    total

--- src/ios/ELCImagePicker/ELCAssetTablePicker.m
+++ src/ios/ELCImagePicker/ELCAssetTablePicker.m
@@ -32,7 +32,7 @@
         //So that we don't have a divide by 0 scenario
         self.columns = 4;
         self.assetPadding = 2;
-        self.total = 49;
+        //self.total = 49;
     }
     return self;
 }
@@ -188,7 +188,7 @@
     //self.total = [[options objectForKey:@"total"] integerValue];
 	
 	//[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.totalSelectedAssets]];
-	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), self.total]];
+	[self.selectionCounter setText:[NSString stringWithFormat:NSLocalizedString(@"Chosen: %d", nil), imagePicker.total]];
 }
 
 - (void)selectAllAction:(id)sender
