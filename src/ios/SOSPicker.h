0eb30737e98fc9bfe7a8d8832962d091054fe766
piratenmedl <mail@bealisa.com>
2017-09-24 09:45:41 +0200

    x

--- src/ios/SOSPicker.h
+++ src/ios/SOSPicker.h
@@ -23,7 +23,5 @@
 @property (nonatomic, assign) NSInteger width;
 @property (nonatomic, assign) NSInteger height;
 @property (nonatomic, assign) NSInteger quality;
-@property (nonatomic, assign) NSInteger vorh;
-@property (nonatomic, assign) NSInteger total;
 
 @end
