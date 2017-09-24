84f7602dd20e9057714e7620d0d3dca00052feb6
piratenmedl <mail@bealisa.com>
2017-09-24 13:01:46 +0200

    x

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -20,16 +20,16 @@
 - (void) getPictures:(CDVInvokedUrlCommand *)command {
 	NSDictionary *options = [command.arguments objectAtIndex: 0];
     NSInteger maximumImagesCount = [[options objectForKey:@"maximumImagesCount"] integerValue];
-    //NSInteger total = [[options objectForKey:@"total"] integerValue];
-    //NSInteger vorh = [[options objectForKey:@"vorh"] integerValue];
+    NSInteger total = [[options objectForKey:@"quality"] integerValue];
+    NSInteger vorh = [[options objectForKey:@"width"] integerValue];
     self.useOriginal = [[options objectForKey:@"useOriginal"] boolValue];
     self.createThumbnail = [[options objectForKey:@"createThumbnail"] boolValue];
     self.saveToDataDirectory = [[options objectForKey:@"saveToDataDirectory"] boolValue];
     self.width = [[options objectForKey:@"width"] integerValue];
     self.height = [[options objectForKey:@"height"] integerValue];
     self.quality = [[options objectForKey:@"quality"] integerValue];
-    self.vorh = [[options objectForKey:@"width"] integerValue];
-    self.total = [[options objectForKey:@"quality"] integerValue];
+    //self.vorh = [[options objectForKey:@"width"] integerValue];
+    //self.total = [[options objectForKey:@"quality"] integerValue];
     
     // Create the an album controller and image picker
     ELCAlbumPickerController *albumController = [[ELCAlbumPickerController alloc] init];
