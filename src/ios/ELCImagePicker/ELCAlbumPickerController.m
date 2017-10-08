30be180822513e89a5fbb60d85b2be8fa8be02e0
piratenmedl <mail@bealisa.com>
2017-10-08 17:17:56 +0200

    crop

--- src/ios/ELCImagePicker/ELCAlbumPickerController.m
+++ src/ios/ELCImagePicker/ELCAlbumPickerController.m
@@ -140,7 +140,8 @@
     cell.textLabel.text = [NSString stringWithFormat:@"%@ (%ld)",[g valueForProperty:ALAssetsGroupPropertyName], (long)gCount];
     UIImage* image = [UIImage imageWithCGImage:[(ALAssetsGroup*)[self.assetGroups objectAtIndex:indexPath.row] posterImage]];
     //image = [self resize:image to:CGSizeMake(70, 70)];
-    image = [self resize:image to:CGSizeMake(0, 0, 80, 80)]; //bea
+    //image = [self resize:image to:CGSizeMake(80, 80)]; //bea
+	image = crop(image!, rect: visibleRect); //bea
     [cell.imageView setImage:image];
 	[cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
 	
